92568c3f1b2c7dedefaa7281198a7a99e73e9306
Hardik Joshi <hardy@teamror.com>
2012-05-04 07:46:15 +0530

    initial commit

--- lib/ckeditor/orm/mongoid.rb
+++ lib/ckeditor/orm/mongoid.rb
@@ -1,4 +1,4 @@
-require 'orm_adapter/adapters/mongoid'
+require 'orm_adapter/adapters/mongo_mapper'
 require 'ckeditor/orm/base'
 
 module Ckeditor
@@ -6,8 +6,8 @@ module Ckeditor
     module Mongoid
       module AssetBase
         def self.included(base)
-          base.send(:include, ::Mongoid::Document)
-          base.send(:include, ::Mongoid::Timestamps)
+          base.send(:include, ::MongoMapper::Document)
+          base.send(:include, ::MongoMapper::Timestamps)
           base.send(:include, Base::AssetBase::InstanceMethods)
           base.send(:include, InstanceMethods)
           base.send(:extend, ClassMethods)
@@ -26,10 +26,10 @@ module Ckeditor
         module ClassMethods
           def self.extended(base)
             base.class_eval do
-              field :data_content_type, :type => String
-              field :data_file_size, :type => Integer
-              field :width, :type => Integer
-              field :height, :type => Integer
+              key :data_content_type,  String
+              key :data_file_size,  Integer
+              key :width,  Integer
+              key :height,  Integer
               
               belongs_to :assetable, :polymorphic => true
               
